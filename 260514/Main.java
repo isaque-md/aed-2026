97925036daa6b7664f1f198a57ac810f7806ced6
isaque-md <i.martins.d19@gmail.com>
2026-05-14 20:12:43 -0300

    mensagem

---
+++ 260514/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static Scanner entrada = new Scanner(System.in);
+
+    public static int lerInteiro(String mensagem) {
+        System.out.println(mensagem);
+        return entrada.nextInt();
+    }
+    public static void main(String[] args){
+        //isaque
+    }
+}
